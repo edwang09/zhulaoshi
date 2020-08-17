30a8e400c1831ca96a1012de6d6380a487a87165
Jianan Wang <edwang09@gmail.com>
2020-08-17 14:20:23 -0400

    Updates

--- bundle.js
+++ bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d796fd96221215b1d4f3";
+/******/ 	var hotCurrentHash = "6a6aa5b5ac01dbeea9af";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -847,7 +847,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/zhulaoshi";
+/******/ 	__webpack_require__.p = "/zhulaoshi/";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
